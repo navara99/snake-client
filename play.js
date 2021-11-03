646554cdea0c485ef969519525730cf9c34b2f23
Tharsikan <thar.navaratnam@gmail.com>
2021-11-03 00:55:08 +0000

    move connect function to client.js and import

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
-const net = require("net");
+const connect = require("./client");
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: "50541",
-  });
-
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log('Message from Server: ' + data);
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
 
 
 
