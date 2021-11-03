5d8d700c81a7d603ca9c18208a4295fdd08121eb
Tharsikan <thar.navaratnam@gmail.com>
2021-11-03 02:39:51 +0000

    added input maps and interval constants

--- constants.js
+++ constants.js
@@ -1,4 +1,28 @@
 const IP = "10.0.2.15";
 const PORT = "50541";
 const INITIALS = "TN";
+const INTERVAL = 50;
 
+const DIRECTIONSMAP = {
+  w: "up",
+  a: "left",
+  s: "down",
+  d: "right"
+};
+
+const MESSAGESMAP = {
+  "?": "Built different",
+  h: "Hello there.",
+  l: "Lets play!",
+  n: "I am snek.",
+  p: ":)"
+};
+
+module.exports = {
+  IP,
+  PORT,
+  INITIALS,
+  DIRECTIONSMAP,
+  MESSAGESMAP,
+  INTERVAL
+}
