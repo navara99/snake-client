311ec1a4738fba21ffec705d04747b2b58863180
Tharsikan <thar.navaratnam@gmail.com>
2021-11-03 17:14:56 +0000

    clear previous setInterval

--- input.js
+++ input.js
@@ -1,22 +1,24 @@
 const { MESSAGESMAP, DIRECTIONSMAP, INTERVAL } = require("./constants")
 
 let connection;
+let interval;
+
+const movement = (input) => {
+  if (interval) clearInterval(interval);
+  interval = setInterval(() => {
+    connection.write("Move: " + input);
+  }, INTERVAL);
+}
 
 const handleUserInput = (input) => {
-
   if (input === '\u0003') {
     console.log("Thank you for playing.")
     process.exit();
   };
 
-  if (DIRECTIONSMAP[input]) {
-    setInterval(() => {
-      connection.write("Move: " + DIRECTIONSMAP[input]);
-    }, INTERVAL);
-  }
+  if (DIRECTIONSMAP[input]) return movement(DIRECTIONSMAP[input]);
 
   if (MESSAGESMAP[input]) connection.write("Say: " + MESSAGESMAP[input]);
-
 }
 
 const setupInput = function (conn) {
