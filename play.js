d4cad1ae43b4a1c9b8237e63ffc392d2ef5e975d
Tharsikan <thar.navaratnam@gmail.com>
2021-11-03 01:30:59 +0000

    moving out setupInput and importing

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
 const connect = require("./client");
-
-const handleUserInput = (input) => {
-  if (input === '\u0003') {
-    process.exit();
-  };
-}
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
+const setupInput = require("./input");
 
 connect();
 setupInput();
