064061fb2b165d48338f33e1c3e9d0bb6fdeb3e1
Tharsikan <thar.navaratnam@gmail.com>
2021-11-03 01:28:18 +0000

    implement exit game

--- play.js
+++ play.js
@@ -1,7 +1,21 @@
 const connect = require("./client");
 
+const handleUserInput = (input) => {
+  if (input === '\u0003') {
+    process.exit();
+  };
+}
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
 
+  stdin.on("data", handleUserInput);
 
+  return stdin;
+};
 
-
+connect();
+setupInput();
